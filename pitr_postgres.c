2227b42fb9f7417dcbe96c0ec9e2854669fb4817
Ramon Basto <42789151+ramonbcll@users.noreply.github.com>
2019-11-28 17:57:20 -0300

    Update pitr_postgres.c

--- pitr_postgres.c
+++ pitr_postgres.c
@@ -4,17 +4,20 @@
 
 int main() {
 	int n;
-	int i = 0;
 
 	//while() {
 	printf("0 - Sair\n");
-	printf("1 - Backup\n");
-	printf("2 - Restaurar.\n");
-	printf("3 - Dump\n");
+	printf("1 - Arquivar logs\n");
+	printf("2 - Backup\n");
+	printf("3 - Restaurar.\n");
+	printf("4 - Dump\n");
 
 	scanf("%d", &n);
 	//if(n == 0) break;
 	if(n == 1) {
+		system("psql -c 'select pg_switch_wal();'");
+	}
+	else if(n == 2) {
 		if(!system("find db_file_backup -type d -empty")) {
 			printf("O backup já foi criado anteriormente.\n");
 		}
@@ -22,24 +25,12 @@ int main() {
 			system("pg_basebackup -Ft -D /var/lib/postgresql/db_file_backup");
 		}
 	}
-	else if(n == 2) {
+	else if(n == 3) {
 		system("tar xvf /var/lib/postgresql/db_file_backup/base.tar -C /var/lib/postgresql/11/main/");
 		system("tar xvf /var/lib/postgresql/db_file_backup/pg_wal.tar -C /var/lib/postgresql/11/main/pg_wal/");
 	}
-	else if(n == 3) {
-		while(n == 3) {
-			system("pg_dumpall > db.out");
-			printf("Realizando dumpall.\n");
-			system("sleep 1");
-			i += 1;
-			if(i == 10) {
-				printf("Ctrl + C para pausar e digite qualquer valor para finalizar.\n");
-				system("PAUSE");
-				//system("sleep 5") && 
-				scanf("%d", &n);
-				i = 0;
-			}
-		}
+	else if(n == 4) {
+		system("pg_dumpall > db.out");
 	}
 
 	//}
