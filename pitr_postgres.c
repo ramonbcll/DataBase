8d00cf822d1de3dacd948b4f914b67ae66e5959b
Ramon Basto <42789151+ramonbcll@users.noreply.github.com>
2019-11-28 10:06:30 -0800

    Update pitr_postgres.c

--- pitr_postgres.c
+++ pitr_postgres.c
@@ -1,24 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
 	int n;
+	int i = 0;
 
 	//while() {
 	printf("0 - Sair\n");
-	printf("1 - Fazer backup do cluster\n");
+	printf("1 - Backup\n");
+	printf("2 - Restaurar.\n");
+	printf("3 - Dump\n");
 
 	scanf("%d", &n);
 	//if(n == 0) break;
-	if(n == 1){
+	if(n == 1) {
 		if(!system("find db_file_backup -type d -empty")) {
-			printf("O backup já foi criado anteriormente\n");
+			printf("O backup já foi criado anteriormente.\n");
 		}
 		else {
 			system("pg_basebackup -Ft -D /var/lib/postgresql/db_file_backup");
 		}
 	}
-	else if(n == 2)
+	else if(n == 2) {
+		system("tar xvf /var/lib/postgresql/db_file_backup/base.tar -C /var/lib/postgresql/11/main/");
+		system("tar xvf /var/lib/postgresql/db_file_backup/pg_wal.tar -C /var/lib/postgresql/11/main/pg_wal/");
+	}
+	else if(n == 3) {
+		while(n == 3) {
+			system("pg_dumpall > db.out");
+			printf("Realizando dumpall.\n");
+			system("sleep 1");
+			i += 1;
+			if(i == 10) {
+				printf("Ctrl + C para pausar e digite qualquer valor para finalizar.\n");
+				system("PAUSE");
+				//system("sleep 5") && 
+				scanf("%d", &n);
+				i = 0;
+			}
+		}
+	}
 
 	//}
 
