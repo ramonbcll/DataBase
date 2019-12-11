3875323da7b5a4654c7bda88c206a912088803df
Ramon Basto <42789151+ramonbcll@users.noreply.github.com>
2019-12-11 13:53:24 -0300

    Update pitr_postgres.c

--- pitr_postgres.c
+++ pitr_postgres.c
@@ -14,11 +14,13 @@ int main() {
 	scanf("%d", &n);
 	//if(n == 0) break;
 	if(n == 1) {
+		system("sudo cp -Rfv /var/lib/postgresql/11/main/pg_wal/archive_status /Documents");
 		system("psql -c 'select pg_switch_wal();'");
 	}
 	else if(n == 2) {
 		system("rm /var/lib/postgresql/db_file_backup/* -r");
-		system("pg_basebackup -Ft -D /var/lib/postgresql/db_file_backup");
+		//system("pg_basebackup -Ft -D /var/lib/postgresql/db_file_backup");
+		system("pg_basebackup -Ft -D /var/lib/postgresql/backup");
 	}
 	else if(n == 3) {
 		system("pg_dumpall > db.out");
