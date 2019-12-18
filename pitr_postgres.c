102c9d2bac3bbcac1063ac36aac763fac65a9779
Ramon Basto <42789151+ramonbcll@users.noreply.github.com>
2019-12-18 15:21:46 -0300

    Update pitr_postgres.c

--- pitr_postgres.c
+++ pitr_postgres.c
@@ -4,29 +4,28 @@
 int main() {
 	int n;
 
-	//while() {
-	printf("0 - Sair\n");
-	printf("1 - Arquivar logs\n");
-	printf("2 - Backup full\n");
-	printf("3 - Dump\n");
+	while(1) {
+		printf("0 - Sair\n");
+		printf("1 - Arquivar logs\n");
+		printf("2 - Backup full\n");
+		printf("3 - Dump\n");
 
-	scanf("%d", &n);
-	//if(n == 0) break;
-	if(n == 1) {
-		system("sudo cp -rfv /var/lib/postgresql/pg_log_archive /home/nti/Documents");
-		system("rm /var/lib/postgresql/pg_log_archive/* -r");
-		system("psql -c 'select pg_switch_wal();'");
+		scanf("%d", &n);
+		if(n == 0) break;
+		else if(n == 1) {
+			system("sudo cp -rfv /var/lib/postgresql/pg_log_archive /home/nti/Documents");
+			system("rm /var/lib/postgresql/pg_log_archive/* -r");
+			system("psql -c 'select pg_switch_wal();'");
+		}
+		else if(n == 2) {
+			system("rm /var/lib/postgresql/db_file_backup/* -r");
+			system("pg_basebackup -Ft -D /var/lib/postgresql/db_file_backup");
+			printf("Backup realizado\n");
+		}
+		else if(n == 3) {
+			system("pg_dumpall > db.out");
+		}
 	}
-	else if(n == 2) {
-		system("rm /var/lib/postgresql/db_file_backup/* -r");
-		system("pg_basebackup -Ft -D /var/lib/postgresql/db_file_backup");
-		//system("pg_basebackup -Ft -D /var/lib/postgresql/backup");
-	}
-	else if(n == 3) {
-		system("pg_dumpall > db.out");
-	}
-
-	//}
 
 	return 0;
 }
